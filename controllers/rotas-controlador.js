e11224dfb925201b05f43fc1af0548f5f32f3e47
frank <hicarojbs21@gmail.com>
2021-03-04 00:09:19 -0300

    leitura de csv, function python nao funcional

--- controllers/rotas-controlador.js
+++ controllers/rotas-controlador.js
@@ -1,13 +1,42 @@
 const templates = require('../views/index');
 require('marko/node-require').install();
-let { PythonShell } = require('python-shell')
+let { PythonShell } = require('python-shell');
+
+function python(resp, dataToSend) {
+    let pyshell = new PythonShell('calculoPk.py');
+
+    let data = [];
+    let x = [];
+    pyshell.send(JSON.stringify(dataToSend));
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        message = JSON.parse(message)
+        console.log(message);
+        console.log("Numero gerado randomicamente: ", message["Numero_gerado_randomicamente"])
+        x = message["strings"];
+        data = message["Pks"];
+
+    });
+    // end the input stream and allow the process to exit
+    pyshell.end(function (err, code, signal) {
+        if (err) throw err;
+        console.log('The exit code was: ' + code);
+        console.log('The exit signal was: ' + signal);
+        console.log('finished');
+        console.log("Data:", data);
+        return resp.status(200).marko(templates.graph, {
+            dados: data,
+            strings: x
+        });
+    });
+}
 
 class RotasControlador {
     static rotas() {
         return {
             graph: '/graph',
             home: '/',
-            upload: "/upload"
+            upload: '/upload'
         };
     }
 
@@ -41,33 +70,7 @@ class RotasControlador {
             });
             dataToSend = aux;
             console.log(dataToSend);
-            let pyshell = new PythonShell('calculoPk.py');
-
-            let data = [];
-            let x = [];
-            pyshell.send(JSON.stringify(dataToSend));
-            pyshell.on('message', function (message) {
-                // received a message sent from the Python script (a simple "print" statement)
-                message = JSON.parse(message)
-                console.log(message);
-                console.log("Numero gerado randomicamente: ", message["Numero_gerado_randomicamente"])
-                x = message["strings"];
-                data = message["Pks"];
-
-            });
-            // end the input stream and allow the process to exit
-            pyshell.end(function (err, code, signal) {
-                if (err) throw err;
-                console.log('The exit code was: ' + code);
-                console.log('The exit signal was: ' + signal);
-                console.log('finished');
-                console.log("Data:", data);
-                return resp.status(200).marko(templates.graph, {
-                    dados: data,
-                    strings: x
-                });
-            });
-
+            return python(resp, dataToSend);
         };
     }
     upload() {
@@ -75,19 +78,45 @@ class RotasControlador {
             console.log("body ", req.body);
             console.log("dir", ".." + __dirname);
             //var formidable = require('formidable');
-            var fs = require('fs');
-            var util = require('util');
+            const fs = require('fs');
             const formidable = require('formidable');
+            const neatCsv = require('neat-csv');
 
-            const form = formidable({ multiples: true, uploadDir: "./"});
+            const form = formidable({ multiples: true, uploadDir: "./", filename: "teste.csv" });
+
+            form.on('fileBegin', (formName, file) => {
+                file.path = "dados.csv"; // to change the final path
+            });
 
             form.parse(req, (err, fields, files) => {
-                console.log(fields, files);
+                //console.log(fields, files);
                 if (err) {
                     next(err);
                     return;
                 }
-                resp.json({ fields, files });
+                //resp.json({ fields, files });
+            });
+
+            fs.readFile('dados.csv', async (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                await neatCsv(data);
+                var csvData = [];
+                const parse = require('csv-parse');
+                fs.createReadStream('dados.csv')
+                    .pipe(parse({ delimiter: ' ' || ';' }))
+                    .on('data', function (csvrow) {
+                        console.log(csvrow);
+                        //do something with csvrow
+                        csvData.push(String(csvrow).replace(/,/, '.'));
+                    })
+                    .on('end', function () {
+                        //do something with csvData
+                        console.log(csvData);
+                    });
+                return python(resp, csvData);
             });
         };
     }
